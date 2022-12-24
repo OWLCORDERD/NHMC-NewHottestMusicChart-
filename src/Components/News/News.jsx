65cde29654fabb3796d096bfccdbe22f44055216
MIYEOK <lim5570@naver.com>
2022-12-24 23:34:07 +0900

    12.24 commit

--- src/Components/News/News.jsx
+++ src/Components/News/News.jsx
@@ -15,6 +15,25 @@ const News = () => {
             <p>최근 화제되고 있는 앨범의 뉴스를 업데이트하는 피드입니다.</p>
         </div>
 
+        <div className = "News-Peed">
+          {data.News.map((item)=>{
+            return(
+          <div className = "Peed-item">
+            <div className = "Peed-image">
+            <img src = {item.image} alt = ""/>
+            </div>
+            
+            <div className = "Peed-content">
+              <div className = "Peed-title">
+                <h1>{item.content}</h1>
+                <p>{item.date}</p>
+              </div>
+              </div>
+          </div>
+          
+            )
+        })}
+        </div>
         
 
     </div>
